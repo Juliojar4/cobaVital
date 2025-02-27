3300d37ec2ae0d8e3c118e3fcdd0aa3a7be0b577
Edson Luiz <177874526+EdsonLuiz-Dev@users.noreply.github.com>
2025-02-27 15:04:02 -0300

    [FEAT] Footer Changes

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="fluid-container bg-blue">
-  <div class="lg:max-w-[1000px] lg:px-0 lg:py-[50px] mx-auto pt-[25px] pb-[20px] px-[10px] flex">
-    <div class="w-1/5 flex items-start">
+  <div class="md:max-w-[1000px] md:px-3 lg:py-[50px] mx-auto pt-[25px] pb-[20px] px-3 flex flex-col md:flex-row gap-4">
+    <div class="md:w-1/5 flex md:items-start md:justify-start justify-center">
       <a class="block h-auto" href="https://www.abbottbrasil.com.br/" target="_blank">
         <img class="object-contain" src="@asset('images/Abbott2.png')" alt="Abbot Logo">
       </a>
     </div>
-    <div class="w-1/5 flex">
+    <div class="md:w-1/5 flex md:my-0 my-3">
       @if (has_nav_menu('footer_menu_1'))
         {!! wp_nav_menu([
           'theme_location' => 'footer_menu_1',
@@ -14,7 +14,7 @@
         ]) !!}
       @endif
     </div>
-    <div class="w-1/5 flex">
+    <div class="md:w-1/5 flex md:my-0 my-3">
       @if (has_nav_menu('footer_menu_2'))
         {!! wp_nav_menu([
           'theme_location' => 'footer_menu_2',
@@ -23,7 +23,7 @@
         ]) !!}
       @endif
     </div>
-    <div class="w-1/5 flex">
+    <div class="md:w-1/5 flex md:my-0 my-3">
       @if (has_nav_menu('footer_menu_3'))
         {!! wp_nav_menu([
           'theme_location' => 'footer_menu_3',
@@ -31,7 +31,7 @@
           'echo' => false,
         ]) !!}
       @endif</div>
-    <div class="w-1/5 flex flex-row gap-6">
+    <div class="md:w-1/5 flex flex-row gap-6 md:my-0 my-3">
       <a href="https://web.facebook.com/AbbottBrasil/" target="_blank">
         <img class="max-h-[35px]" src="@asset('images/facebook-white-logo.png')" alt="Facebook Logo">
       </a>
@@ -40,7 +40,7 @@
       </a>
     </div>
   </div>
-  <div class="mx-auto max-w-[1000px] pb-[60px]">
+  <div class="mx-auto max-w-[1000px] pb-[60px] px-3">
     <p class="text-xs text-white">Preferências de Cookies</p>
   </div>
 </footer>
