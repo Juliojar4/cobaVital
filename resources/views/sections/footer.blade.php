5f24c7b83b0a88e5491cf2bd7afe143807e94ca3
Edson Luiz <177874526+EdsonLuiz-Dev@users.noreply.github.com>
2025-02-27 15:08:04 -0300

    [FEAT] Footer Changes

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="fluid-container bg-blue">
-  <div class="md:max-w-[1000px] md:px-3 lg:py-[50px] mx-auto pt-[25px] pb-[20px] px-3 flex flex-col md:flex-row gap-4">
+  <div class="md:max-w-[1000px] md:px-3 md:flex-row lg:py-[50px] mx-auto pt-[25px] pb-[20px] px-3 gap-4 flex flex-col">
     <div class="md:w-1/5 flex md:items-start md:justify-start justify-center">
       <a class="block h-auto" href="https://www.abbottbrasil.com.br/" target="_blank">
         <img class="object-contain" src="@asset('images/Abbott2.png')" alt="Abbot Logo">
       </a>
     </div>
-    <div class="md:w-1/5 flex md:my-0 my-3">
+    <div class="md:my-0 md:w-1/5 my-3 flex">
       @if (has_nav_menu('footer_menu_1'))
         {!! wp_nav_menu([
           'theme_location' => 'footer_menu_1',
@@ -14,7 +14,7 @@
         ]) !!}
       @endif
     </div>
-    <div class="md:w-1/5 flex md:my-0 my-3">
+    <div class="md:my-0 md:w-1/5 my-3 flex">
       @if (has_nav_menu('footer_menu_2'))
         {!! wp_nav_menu([
           'theme_location' => 'footer_menu_2',
@@ -23,7 +23,7 @@
         ]) !!}
       @endif
     </div>
-    <div class="md:w-1/5 flex md:my-0 my-3">
+    <div class="md:w-1/5 md:my-0 flex my-3">
       @if (has_nav_menu('footer_menu_3'))
         {!! wp_nav_menu([
           'theme_location' => 'footer_menu_3',
@@ -31,7 +31,7 @@
           'echo' => false,
         ]) !!}
       @endif</div>
-    <div class="md:w-1/5 flex flex-row gap-6 md:my-0 my-3">
+    <div class="md:w-1/5 md:my-0 flex flex-row gap-6 my-3">
       <a href="https://web.facebook.com/AbbottBrasil/" target="_blank">
         <img class="max-h-[35px]" src="@asset('images/facebook-white-logo.png')" alt="Facebook Logo">
       </a>
